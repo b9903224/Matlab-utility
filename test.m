abf9d9ac22d3f5f98d2ebedeee6298eba1469716
b9903224 <b9903224@gmail.com>
2019-10-29 23:56:57 +0800

    Add files via upload

---
+++ test.m
@@ -0,0 +1,17 @@
+%% Ming-Chih, Hsiao
+close all
+format compact
+
+%% getEllipseBwImg
+imgH = 512;
+imgW = 1024;
+ellipseBwImg = [];
+ellipseBwImg = Utility.getEllipseBwImg(imgH, imgW, 256, 512, 64, 128, 15, ellipseBwImg);
+ellipseBwImg = Utility.getEllipseBwImg(imgH, imgW, 128, 256, 32, 64, -30, ellipseBwImg);
+ellipseBwImg = Utility.getEllipseBwImg(imgH, imgW, 256+128, 256, 32, 64, 30, ellipseBwImg);
+ellipseBwImg = Utility.getEllipseBwImg(imgH, imgW, 512, 256+512, 512, 64, 30, ellipseBwImg);
+ellipseBwImg = Utility.getEllipseBwImg(imgH, imgW, 512, 256, 64, 512, 0, ellipseBwImg); 
+ellipseBwImg = Utility.getEllipseBwImg(imgH, imgW, 256, 90, 128, 64, 0, ellipseBwImg); 
+ellipseBwImg = Utility.getEllipseBwImg(imgH, imgW, 87, 685, 512, 30, -45, ellipseBwImg); 
+figure, imshow(ellipseBwImg)
+imwrite(ellipseBwImg, 'ellipseBwImg_1.png')
